74cc532be7a4c654e938f44d46a04930dcb540d1
Nguyen Trong Hieu <hieunguyen6231@gmail.com>
2025-07-16 16:14:24 +0700

    config alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import Test from '@components/test/Test';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello Ecommerse</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>
+                <Test /> Ecommerse
+            </h1>
+        </div>
+    );
 }
 
 export default App;
